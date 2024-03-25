d41648e4a7139920b0588096e93387c1cb30bf64
blackxavier <ogbuchiarinze01@gmail.com>
2024-03-25 21:40:51 +0100

    Fix #7

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,13 @@ import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
 import icon from "astro-icon";
 import { defineConfig } from 'astro/config';
+// https://astro.build/config
+
 export default defineConfig({
+  integrations: [tailwind(), icon(), compress()],,
   site: 'https://blackxavier.github.io'
 })
+
+
+
+
