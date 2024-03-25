9391c2bab9cd2920bf93fb6f1821c3009429f8d9
blackxavier <ogbuchiarinze01@gmail.com>
2024-03-25 20:45:51 +0100

    Fix #4

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,9 +9,3 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   site: 'https://blackxavier.github.io'
 })
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), icon(), compress()],
-  output: "server",
-  adapter: vercel()
-});
